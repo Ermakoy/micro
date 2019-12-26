981959086c9876e15d05eb1db186b06b87354a25
Andrey Ermakov <a.ermakov@corp.vk.com>
2019-12-26 11:36:09 +0300

    CRUD для warehouse'а

--- warehouse/src/index.js
+++ warehouse/src/index.js
@@ -37,80 +37,34 @@ app.use(bodyParser.json())
 
 app.get('/ping', (req, res) => res.send('hello from warehouse'))
 
-app.post(`/user`, async (req, res) => {
-  const result = await photon.users.create({
-    data: {
-      ...req.body,
-    },
-  })
+app.get('/items', async (req,res) => {
+  const result = await photon.items.findMany({})
+
   res.json(result)
 })
 
-app.post(`/post`, async (req, res) => {
-  const { title, content, authorEmail } = req.body
-  const result = await photon.posts.create({
-    data: {
-      title: title,
-      content: content,
-      author: { connect: { email: authorEmail } },
-    },
-  })
+app.get('/items/:id', async (req,res) => {
+  const result = await photon.items.findOne({where: {id: req.params.id}})
+
   res.json(result)
 })
 
-app.put('/publish/:id', async (req, res) => {
-  const { id } = req.params
-  const post = await photon.posts.update({
-    where: { id },
-    data: { published: true },
-  })
-  res.json(post)
+app.post('/items', async (req,res) => {
+  const result = await photon.items.create(req.body);
+
+  res.json(result)
 })
 
-app.delete(`/post/:id`, async (req, res) => {
-  const { id } = req.params
-  const post = await photon.posts.delete({
-    where: {
-      id,
-    },
-  })
-  res.json(post)
+app.put('/items/:id', async (req,res) => {
+  const result = await photon.items.update({where: {id: req.params.id}, data: req.body});
+
+  res.json(result)
 })
 
-app.get(`/post/:id`, async (req, res) => {
-  const { id } = req.params
-  const post = await photon.posts.findOne({
-    where: {
-      id,
-    },
-  })
-  res.json(post)
-})
+app.delete('/items/:id', async (req, res) => {
+  const result = await photon.items.delete({where: {id: req.params.id}})
 
-app.get('/feed', async (req, res) => {
-  const posts = await photon.posts.findMany({ where: { published: true } })
-  res.json(posts)
-})
-
-app.get('/filterPosts', async (req, res) => {
-  const { searchString } = req.query
-  const draftPosts = await photon.posts.findMany({
-    where: {
-      OR: [
-        {
-          title: {
-            contains: searchString,
-          },
-        },
-        {
-          content: {
-            contains: searchString,
-          },
-        },
-      ],
-    },
-  })
-  res.json(draftPosts)
+  res.json(result)
 })
 
 const server = app.listen(3001, () =>
